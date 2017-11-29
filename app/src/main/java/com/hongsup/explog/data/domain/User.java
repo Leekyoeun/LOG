647733bab7e563168340460b4bdd26eb7ec65316
Hooooong <h921013@gmail.com>
2017-11-29 21:55:53 +0900

    SignIn, SingUp Modeling, Retrofit2 Modify

--- app/src/main/java/com/hongsup/explog/data/domain/User.java
+++
@@ -1,9 +0,0 @@
-package com.hongsup.explog.data.domain;
-
-public class User {
-    public String password1;
-    public String username;
-    public String email;
-    public String img_profile;
-    public String password2;
-}
