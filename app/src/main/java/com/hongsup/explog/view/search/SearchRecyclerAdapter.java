966a45f0fabcf5c88977b1289ef923c3590f5cb7
Insup Jung <31408300+jis1218@users.noreply.github.com>
2017-12-11 00:49:40 +0900

    search merge

--- app/src/main/java/com/hongsup/explog/view/search/SearchRecyclerAdapter.java
+++ app/src/main/java/com/hongsup/explog/view/search/SearchRecyclerAdapter.java
@@ -1,6 +1,7 @@
 package com.hongsup.explog.view.search;
 
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,44 +16,72 @@ import java.util.ArrayList;
  * Created by 정인섭 on 2017-12-09.
  */
 
-public class SearchRecyclerAdapter extends RecyclerView.Adapter<SearchRecyclerAdapter.MyHolder>{
+public class SearchRecyclerAdapter extends RecyclerView.Adapter<ViewHolder> {
 
-    private ArrayList<String> list = new ArrayList<>();
-    ListAction listAction;
+    private final static int HISTORY = 111;
+    private final static int RESULT = 222;
+    private final static int NOTHINGTOSEARCH = 333;
+    private int flag = HISTORY;
+
+    private ArrayList<String> historyList = new ArrayList<>();
+    //private ArrayList<Result> resultList = new ArrayList<>();
+    private  ListAction listAction;
 
     public SearchRecyclerAdapter(ListAction listAction) {
         this.listAction = listAction;
     }
 
-    public void notifier(ArrayList<String> list){
-        this.list = list;
+    public void historyNotifier(ArrayList<String> historyList) {
+        this.historyList = historyList;
         notifyDataSetChanged();
+        flag = HISTORY;
+    }
+
+//    public void resultNotifier(ArrayList<Result> resultList) {
+//        this.resultList = resultList;
+//        notifyDataSetChanged();
+//        flag = RESULT;
+//    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        switch (flag) {
+
+            case HISTORY:
+                View historyView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_search_recycler_history, parent, false);
+                return new HistoryHolder(historyView);
+
+            case RESULT :
+                View resultView = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_search_recycler_result, parent, false);
+                return new ResultHolder(resultView);
+        }
+        return null;
     }
 
     @Override
-    public MyHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.view_search_recycler_history, parent, false);
-        return new MyHolder(view);
-    }
-
-    @Override
-    public void onBindViewHolder(MyHolder holder, int position) {
-        holder.textView.setText(list.get(position));
-        holder.word = list.get(position);
-
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        switch (flag) {
+            case HISTORY:
+                ((HistoryHolder) holder).textView.setText(historyList.get(position));
+                ((HistoryHolder) holder).word = historyList.get(position);
+                break;
+            case RESULT :
+        }
     }
 
     @Override
     public int getItemCount() {
-        return list.size();
+        //if(flag==HISTORY)
+        return historyList.size();
+        //else if(flag==RESULT)
     }
 
-    class MyHolder extends RecyclerView.ViewHolder{
+    class HistoryHolder extends ViewHolder {
         TextView textView;
         ImageView imgDeleteHistory;
         String word;
 
-        public MyHolder(View itemView) {
+        public HistoryHolder(View itemView) {
             super(itemView);
             textView = itemView.findViewById(R.id.textViewWord);
             imgDeleteHistory = itemView.findViewById(R.id.imgDeleteHistory);
@@ -72,8 +101,15 @@ public class SearchRecyclerAdapter extends RecyclerView.Adapter<SearchRecyclerAd
         }
     }
 
-    interface ListAction{
+    class ResultHolder extends ViewHolder {
+        public ResultHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    interface ListAction {
         void setHistoryToSearchEditText(String word);
+
         void deleteHistory(String word);
     }
 }
